91844eb203705704f55af44e0cfe621474ff987a
mayurpandit25 <panditmayur888@gmail.com>
2026-07-22 14:45:00 +0530

    day-2

--- day-2/resource.tf
+++ day-2/resource.tf
@@ -1,9 +1,44 @@
+data "aws_vpc" "default" {
+    default = true 
+}
+
+resource "aws_security_group" "sg" {
+    name = "my-security-group"
+    description = "my-security-group"
+    vpc_id = data.aws_vpc.default.id 
+
+    ingress {
+        from_port = 22
+        to_port = 22 
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    tags = {
+        Name = "my-security-group"
+    }
+}
+
 resource "aws_instance" "ec2" {
     ami           = var.ami
     instance_type = var.instance_type
     key_name      = var.key_name 
     disable_api_termination = false 
-    vpc_security_group_ids = [var.sg_id]
+    vpc_security_group_ids = [aws_security_group.sg.id]
     user_data = file("/root/terraform-b32/day-2/user-data.sh")
 
     root_block_device {
