378c3314c3513586b82924c2a1c0787c9aaadcd8
mayurpandit25 <panditmayur888@gmail.com>
2026-07-22 13:52:14 +0530

    day-2

--- day-2/resource.tf
+++ day-2/resource.tf
@@ -2,7 +2,6 @@ resource "aws_instance" "ec2" {
     ami           = "ami-078fe7ff43e10cf8c"
     instance_type = "t3.micro"
     key_name      = "my-key-ohio"
-    count         = 2
     disable_api_termination = false 
     vpc_security_group_ids = ["sg-0c98b6b2580462b01"]
     user_data = file("/root/terraform-b32/day-2/user-data.sh")
