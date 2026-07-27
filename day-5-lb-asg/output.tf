ea60b5ed9d762970b9e508c195def6663ac72746
mayurpandit25 <panditmayur888@gmail.com>
2026-07-27 13:59:45 +0530

    day-5-lb-asg

---
+++ day-5-lb-asg/output.tf
@@ -0,0 +1,4 @@
+output "lb_dns" {
+    value = aws_lb.lb.dns_name
+}
+
---
+++ day-5-lb-asg/var.tf
@@ -0,0 +1,44 @@
+variable "ingress_http" {
+    default = 80
+}
+
+variable "ingress_ssh" {
+    default = 22
+}
+
+variable "tg_port" {
+    default = 80
+}
+
+variable "load_balancer_type" {
+    default = "application"
+}
+
+variable "ami" {
+    default = "ami-0aa939abad20cdeae" 
+}
+
+variable "key_name" {
+    default = "my-key-ohio"
+}
+
+variable "instance_type" {
+    default = "t3.micro"
+}
+
+variable "desired_capacity" {
+    default = 2
+}
+
+variable "min_size" {
+    default = 2
+}
+
+variable "max_size" {
+    default = 5
+}
+
+variable "lt_version" {
+    default = "$Latest"
+}
+
